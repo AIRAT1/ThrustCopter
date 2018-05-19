44cc40963120d8c9e74f72da5208e27ede9ce9c3
Ayrat <ayrat1@mail.ru>
2018-05-19 16:22:37 +0300

    add FpsLogger instance

--- core/src/de/android/ayrathairullin/trustcopter/ThrustCopter.java
+++ core/src/de/android/ayrathairullin/trustcopter/ThrustCopter.java
@@ -2,32 +2,39 @@ package de.android.ayrathairullin.trustcopter;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class ThrustCopter extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private FPSLogger fpsLogger;
+	private SpriteBatch batch;
+
 	@Override
 	public void create () {
+		fpsLogger = new FPSLogger();
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		fpsLogger.log();
+		updateScene();
+		drawScene();
 	}
-	
+
+	private void updateScene() {
+
+	}
+
+	private void drawScene() {
+
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
