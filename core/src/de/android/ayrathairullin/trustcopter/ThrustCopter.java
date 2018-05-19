b64910f9e3bd4a75cc8c6b619b71dee826d8922a
Ayrat <ayrat1@mail.ru>
2018-05-19 17:05:42 +0300

    add terrain and copter

--- core/src/de/android/ayrathairullin/trustcopter/ThrustCopter.java
+++ core/src/de/android/ayrathairullin/trustcopter/ThrustCopter.java
@@ -6,13 +6,17 @@ import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class ThrustCopter extends ApplicationAdapter {
 	private FPSLogger fpsLogger;
 	private SpriteBatch batch;
 	private OrthographicCamera camera;
-	private Texture background;
+	private TextureRegion backgroundRegion, terrainBelow, terrainAbove;
+	private float terrainOffset, planeAnimTime;
+	private Animation<TextureRegion> plane;
 
 	@Override
 	public void create () {
@@ -20,7 +24,15 @@ public class ThrustCopter extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 800,480);
-		background = new Texture(Gdx.files.internal("background.png"));
+		backgroundRegion = new TextureRegion(new Texture("background.png"));
+		terrainBelow = new TextureRegion(new Texture("groundGrass.png"));
+		terrainAbove = new TextureRegion(terrainBelow);
+		terrainAbove.flip(true, true);
+		plane = new Animation<TextureRegion>(.05f, new TextureRegion(new Texture("planeRed1.png")),
+				new TextureRegion(new Texture("planeRed2.png")),
+				new TextureRegion(new Texture("planeRed3.png")),
+				new TextureRegion(new Texture("planeRed2.png")));
+		plane.setPlayMode(Animation.PlayMode.LOOP);
 	}
 
 	@Override
@@ -33,20 +45,30 @@ public class ThrustCopter extends ApplicationAdapter {
 	}
 
 	private void updateScene() {
-
+		terrainOffset -= 200 * Gdx.graphics.getDeltaTime();
+		planeAnimTime += Gdx.graphics.getDeltaTime();
 	}
 
 	private void drawScene() {
 		camera.update();
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(background, 0, 0);
+		batch.disableBlending();
+		batch.draw(backgroundRegion, 0, 0);
+		batch.enableBlending();
+
+		batch.draw(terrainBelow, terrainOffset, 0);
+		batch.draw(terrainBelow, terrainOffset + terrainBelow.getRegionWidth(), 0);
+		batch.draw(terrainAbove, terrainOffset, 480 - terrainAbove.getRegionHeight());
+		batch.draw(terrainAbove, terrainOffset + terrainAbove.getRegionWidth(), 480 - terrainAbove.getRegionHeight());
+
+		batch.draw(plane.getKeyFrame(planeAnimTime), 350, 200);
+
 		batch.end();
 	}
 
 	@Override
 	public void dispose () {
 		batch.dispose();
-		background.dispose();
 	}
 }
--- desktop/src/de/android/ayrathairullin/trustcopter/desktop/DesktopLauncher.java
+++ desktop/src/de/android/ayrathairullin/trustcopter/desktop/DesktopLauncher.java
@@ -2,11 +2,16 @@ package de.android.ayrathairullin.trustcopter.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import de.android.ayrathairullin.trustcopter.ThrustCopter;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.useGL30 = false;
+		config.title = "Thrust Copter";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new ThrustCopter(), config);
 	}
 }
